2ea19232bdfb9b0f439f94bc58ebad452fe0f4c7
Abdoeltawil <abdoeltawil78@gmail.com>
2022-11-02 23:29:59 +0200

    first Commit

---
+++ CourseSystem/Areas/Admin/Controllers/AdminsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CourseSystem.Models;
+
+namespace CourseSystem.Areas.Admin.Controllers
+{
+    public class AdminsController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ CourseSystem/services/Admin.cs
@@ -0,0 +1,55 @@
+﻿using CourseSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace CourseSystem.services
+{
+    interface AdminServices
+    {
+        bool login(string email , string password);
+        bool ChangePassword(string email, string password);
+        bool ForgetPassword(string email);
+    }
+    public class Admin : AdminServices
+    {
+        public OnlineCoursesSystemDBEntities Context { set; get; }
+        public Admin()
+        {
+            Context = new OnlineCoursesSystemDBEntities(); 
+        }
+        
+        public bool ChangePassword(string email, string password)
+        {
+            try
+            {
+
+            Context.Admins.FirstOrDefault(i => i.Email == email).Password=password;
+            return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool ForgetPassword(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool login(string email, string password)
+        {
+            try
+            {
+                if (Context.Admins.Find(email) == null)
+                    return false;
+                var item = Context.Admins.Find(email);
+                return (item.Password == password);
+            }
+            catch { return false; }
+        }
+    }
+}
